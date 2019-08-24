630785aeb6c01c2739ddb5af620411807c1ced8b
hanlinchen <harrychen1995@yahoo.com>
2019-08-23 22:38:20 -0400

    ok

--- queue_multithreading.py
+++ queue_multithreading.py
@@ -5,30 +5,38 @@ from queue import Queue
 number_thread=20
 number_jobs=30
 def func1():
-    time.sleep(1)
+        time.sleep(0.5)
 
 def jobs(q):
-    while not q.empty():
-        task=q.get()
-        task()
+    while True:
+        if q.get() is None:
+                break
+        func1()
         q.task_done()
 
 
 q = Queue()
-for i in range(number_jobs):
-    q.put(func1)
-
-
-
 thread_list =[]
+
+# start threads
 for i in range(number_thread):
     thread_list.append(threading.Thread(target=jobs,args=(q,)))
     thread_list[i].start()
+
 start = time.time()
 
+
+for i in range(number_jobs):
+    q.put(i)
+
+# wait untill all jons in queue is done
 q.join()
+
+# stop threads
 for i in range(number_thread):
-    thread_list[i].join()
+    q.put(None)
+for t in thread_list:
+    t.join()
 
 print("time finish {} in seconds with multithreading".format(time.time()-start))
 
