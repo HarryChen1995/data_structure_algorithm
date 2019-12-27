5a0284d0d04005b9357bf838ca98b7eefaaed00f
Harry Chen <HarryChen1995@yahoo.com>
2019-12-27 15:10:34 -0500

    Add files via upload

---
+++ Graph.py
@@ -0,0 +1,71 @@
+from collections import defaultdict
+
+class Graph:
+        def __init__(self):
+            self.graph = defaultdict(list)
+        def add_edge(self, A, B):
+            if B not in self.graph[A]:
+                self.graph[A].append(B)
+            else:
+                raise Exception("Node " + B + " is already connected to Node  "+A)
+
+
+        def __str__(self):
+            return str(dict(self.graph))
+
+        def DBS_Recursive(self, V, path, visited = defaultdict(lambda:False)):
+            visited[V] = True 
+            path.append(V)
+            for i in self.graph[V]:
+                if not visited[i]:
+                    self.DBS_Recursive(i, path, visited)
+        def DBS_Stack(self, V):
+            path = []
+            vistied = defaultdict(lambda:False)
+            Stack = [V]
+            while len(Stack):
+                k = Stack.pop()
+                if not vistied[k]:
+                    path.append(k)
+                    vistied[k] = True
+
+                for i in self.graph[k]:
+                    if not vistied[i]:
+                        Stack.append(i)
+            return path
+        def DBS_Queue(self, V):
+            path = []
+            vistied = defaultdict(lambda:False)
+            Queue = [V]
+            while len(Queue):
+                k = Queue.pop(0)
+                if not vistied[k]:
+                    path.append(k)
+                    vistied[k] = True
+
+                for i in self.graph[k]:
+                    if not vistied[i]:
+                        Queue.append(i)
+            return path
+
+
+
+
+
+
+
+            
+
+
+
+
+g = Graph()
+g.add_edge("A", "B")
+g.add_edge("A", "C")
+g.add_edge("C", "D")
+g.add_edge("C", "E")
+path = []
+g.DBS_Recursive("A", path)
+print(path)
+print(g.DBS_Stack("A"))
+print(g.DBS_Queue("A"))
