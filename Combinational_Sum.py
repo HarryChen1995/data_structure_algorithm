e420ae181c1162a67199a101814f054a8c216cec
harrychen <harrychen1995@yahoo.com>
2020-01-07 21:25:24 -0500

    ok

--- Combinational_Sum.py
+++ Combinational_Sum.py
@@ -1,5 +1,4 @@
-
-
+#method 1
 def combinational_sum(arr, Sum, result, start, r):
     if Sum ==0:
         result.append(r)
@@ -7,11 +6,6 @@ def combinational_sum(arr, Sum, result, start, r):
     for i in range(start, len(arr)):
         if Sum - arr[i]>=0:
             combinational_sum(arr,Sum-arr[i], result,i,r+[arr[i]])
-
-
-
-
-
 
 def find_combinational_sum(arr, s):
     arr = sorted(list(set(arr)))
@@ -24,3 +18,25 @@ def find_combinational_sum(arr, s):
 
 
 print(find_combinational_sum([2,4,6,8],10))
+
+
+
+# method 2 
+def combinationalsum(x,s,i,r,result):
+        if s == 0:
+            result.append(r)
+        else:
+            if i<len(x):
+                combinationalsum(x,s-x[i], i+1, r+[x[i]], result)
+                combinationalsum(x, s, i+1, r, result)
+
+x = [1,2,3,4,5,6]
+s= 12
+r = []
+result = []
+combinationalsum(x,s,0,r,result)
+print(result)
+
+        
+
+    
