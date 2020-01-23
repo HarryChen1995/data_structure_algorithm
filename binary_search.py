6172a90c3f9d9d2cb76fa1e1312f1f1d4af2b589
Harry Chen <HarryChen1995@yahoo.com>
2020-01-23 17:29:05 -0500

    Add files via upload

--- binary_search.py
+++ binary_search.py
@@ -6,7 +6,7 @@ def linear_search(arr, k):
     
     return (False,None)
 
-def binary_search(arr, left, right, k):
+def binary_search1(arr, left, right, k):
     if left > right:
         return (False, None)
      
@@ -16,16 +16,33 @@ def binary_search(arr, left, right, k):
             return (True, mid)
     
     elif k > arr[mid]:
-        return binary_search(arr, mid+1, right,k)
+        return binary_search1(arr, mid+1, right,k)
     
     else:
-        return binary_search(arr, left, mid-1,k)
+        return binary_search1(arr, left, mid-1,k)
 
 
+def binary_search2(arr,data):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = low + (high-low)//2
+        if  arr[mid] == data:
+            return (True, mid)
+        elif data < arr[mid]:
+            high = mid-1
+        else:
+            low = mid+1
+    return (False, None)
 
 
-
-x =  [1,2,3,4,4,10]
+x = range(1, 10000) 
 current_time = time.time()
-print(binary_search(x, 0, len(x)-1,4))
-print("binary_search takes {} seconds".format(time.time()-current_time))
+print(binary_search1(x, 0, len(x)-1,6323))
+print("binary_search1 takes {} seconds".format(time.time()-current_time))
+current_time = time.time()
+print(binary_search2(x,6323))
+print("binary_search2 takes {} seconds".format(time.time()-current_time))
+current_time = time.time()
+print(linear_search(x,6323))
+print("linear_search takes {} seconds".format(time.time()-current_time))
